805cc55392995bdc6d19f8db707fd967843bcc72
banafys <22682230+Banafys@users.noreply.github.com>
2020-01-14 14:18:34 -0500

    Fix error in create profile function.

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,31 @@ const admin = require('firebase-admin');
 // });
 admin.initializeApp();
 
+exports.createPublicProfile = functions.https.onCall(async (data, context) => {
+    checkAuthentication(context);
+    dataValidator(data, { user: 'string' })
+
+    const userProfile = await admin.firestore().collection('publicProfiles')
+        .where('userId', '==', context.auth.uid)
+        .limit(1)
+        .get();
+
+    if (!userProfile.empty) {
+        throw new functions.https.HttpsError('already-exists', 'This user already has a public profile.')
+    }
+
+    const publicProfile = await admin.firestore().collection('publicProfiles').doc(data.user).get();
+    if (publicProfile.exists) {
+        throw new functions.https.HttpsError('already-exists', 'This username already belongs to an existing user.')
+    }
+
+    return admin.firestore().collection('publicProfiles').doc(data.user).set({
+        userId: context.auth.uid
+    });
+});
+
+
+
 exports.postComment = functions.https.onCall((data, context) => {
     checkAuthentication(context);
     dataValidator(data, {
